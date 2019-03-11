26d5d023493b8eed6cdd431ed1e432d583d78dd7
Arunav Gupta <arunav1101@gmail.com>
2019-03-10 22:42:42 -0700

    learning

--- assets/logic/app.js
+++ assets/logic/app.js
@@ -38,3 +38,64 @@ $(`#searchBtn`).on("click", function () {
         })
     });
 });
+// Logic to get location
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        var x = document.getElementById("location");
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    var x = document.getElementById("location");
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+
+    $.ajax({
+      type:"GET",
+      url:"https://app.ticketmaster.com/discovery/v2/events?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&size=2&latlong="+latlon,
+      async:true,
+      dataType: "json",
+      success: function(json) {
+                  console.log(json);
+                  var e = document.getElementById("events");
+                  e.innerHTML = json.page.totalElements + " events found.";
+                  showEvents(json);
+               },
+      error: function(xhr, status, err) {
+                  console.log(err);
+               }
+    });
+
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
+function showEvents(json) {
+  for(var i=0; i<json.page.size; i++) {
+    $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
+  }
+}
+
+
+getLocation();
