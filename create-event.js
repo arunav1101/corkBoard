b28e68af7cfe4aaad1cfa4f565b54b8532b40bbd
tdj120 <tdj120@gmail.com>
2019-03-12 18:56:39 -0700

    temp create-event page

--- create-event.js
+++ create-event.js
@@ -1,24 +1,24 @@
 $(function(){
 
-   var config = {
-      apiKey: "AIzaSyD0cTV847nOsyMKkChLqVZSxTD6OMoucXw",
-      authDomain: "volex-da21a.firebaseapp.com",
-      databaseURL: "https://volex-da21a.firebaseio.com",
-      projectId: "volex-da21a",
-      storageBucket: "volex-da21a.appspot.com",
-      messagingSenderId: "986439603302"
-   }
+   let config = {
+      apiKey: "AIzaSyBbdB0Z13saO2C5CbwRn6sNvyR9jFuYrtA",
+      authDomain: "eventsearch-9871b.firebaseapp.com",
+      databaseURL: "https://eventsearch-9871b.firebaseio.com",
+      projectId: "eventsearch-9871b",
+      storageBucket: "eventsearch-9871b.appspot.com",
+      messagingSenderId: "329903550973"
+    };
 
    firebase.initializeApp(config);
 
-   var newEventName = "";
-   var newEventLocation = "";
-   var newEventDate = "";
-   var newEventTime = "";
-   var newEventDescription = "";
-   var userName= "user"
+   let newEventName = "";
+   let newEventLocation = "";
+   let newEventDate = "";
+   let newEventTime = "";
+   let newEventDescription = "";
+   let userName= "user"
 
-   var database = firebase.database().ref('Events/'+newEventName);
+   let database = firebase.database().ref('Events/'+newEventName);
 
 
   
@@ -27,8 +27,10 @@ $(function(){
 
        newEventName=$("#event-name-input").val();
        newEventLocation=$("#event-location-input").val();
-       newEventDate=$("#event-date-input").val();
-       newEventTime=$("#event-time-input").val();
+       newEventStartDate=$("#event-date-start-input").val();
+       newEventStartTime=$("#event-time-start-input").val();
+       newEventEndDate=$("#event-date-end-input").val();
+       newEventEndTime=$("#event-time-end-input").val();
        newEventDescription=$("#event-description-input").val();
        
 
@@ -36,13 +38,14 @@ $(function(){
 
       database.set({
 
-         name: newEventName,
-         location: newEventLocation,
-         date: newEventDate,
-         time: newEventTime,
-         description: newEventDescription,
-         dateAdded: firebase.database.ServerValue.TIMESTAMP,
-         username: userName
+         Name: newEventName,
+         Zipcode: newEventLocation,
+         Start: newEventDate,
+         Expire: newEvent
+         StartTime: newEventTime,
+         Description: newEventDescription,
+         DateAdded: firebase.database.ServerValue.TIMESTAMP,
+         Username: userName
 
       });
     });
@@ -59,13 +62,13 @@ $(function(){
 
    database.orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
       
-      var newEventCard = $("<card>");
+      let newEventCard = $("<card>");
 
-      var cardName = $("<h3>").text(snapshot.val().name);
-      var cardLocation = $("<h4>").text(snapshot.val().location);
-      var cardDate = $("<h4>").text(snapshot.val().date);
-      var cardTime = $("<h4>").text(snapshot.val().time);
-      var cardDescription = $("<h4>").text(snapshot.val().description);
+      let cardName = $("<h3>").text(snapshot.val().name);
+      let cardLocation = $("<h4>").text(snapshot.val().location);
+      let cardDate = $("<h4>").text(snapshot.val().date);
+      let cardTime = $("<h4>").text(snapshot.val().time);
+      let cardDescription = $("<h4>").text(snapshot.val().description);
 
       newEventCard.append(cardName, cardLocation, cardDate, cardTime, cardDescription);
       $("#created-events-here").append(newEventCard);
